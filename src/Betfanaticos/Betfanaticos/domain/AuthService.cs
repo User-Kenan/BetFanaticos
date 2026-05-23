7aace663e8ea3f27c71020a25043294014d9b43d
Emir ALICI <emir.alici@student.htl-rankweil.at>
2026-05-23 13:35:09 +0200

    Authservice Methode Login Register implementiert und etwas an Xaml gearbeitet

---
+++ src/Betfanaticos/Betfanaticos/domain/AuthService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Betfanaticos.domain
+{
+    public class AuthService
+    {
+        private User registeredUser;
+
+        public User Register(string username, string password)
+        {
+
+            registeredUser = new User();
+            registeredUser.Username = username;
+            registeredUser.Password = password; 
+
+            return registeredUser;
+        }
+
+        public bool Login(string username, string password)
+        {
+            
+            
+            if (registeredUser.Username == username && registeredUser.Password == password && registeredUser != null)
+            {
+                return true;
+            }
+            
+
+            else 
+            {
+                return false;
+            }
+            
+        }
+
+        public void HashPassword(string password)
+        {
+            // TODO
+        }
+    }
+}
