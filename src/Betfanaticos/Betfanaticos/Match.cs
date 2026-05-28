71cb665a43f3a99eafa02054b86a8338e09764f1
Dell <Dell@DESKTOP-4MOTUUD>
2026-05-28 10:39:56 +0200

    User Klasse, Match Klasse sowie enum MatchStatus und enum Sportype erledigt. Bet Klasse erstellt doch noch nicht angefangen

---
+++ src/Betfanaticos/Betfanaticos/Match.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Betfanaticos
+{
+    public class Match
+    {
+        public int Id { get; set; } 
+        public string HomeTeam { get; set; }
+        public string AwayTeam { get; set; }
+        public DateTime MatchDate { get; set; }
+        public string League { get; set; }
+        public int AwayScore { get; set; }
+        public int HomeScore { get; set; }
+        public SportType Sport {  get; set; }
+        public MatchStatus Status { get; set;  } 
+        
+        
+        public Match(string hometeam, string awayteam, string league, SportType sport, DateTime matchdate)
+        {
+            HomeTeam = hometeam;
+            AwayTeam = awayteam;
+            League = league;
+            Sport = sport;
+            MatchDate = matchdate;
+        }
+            
+    
+    
+    }
+
+
+}
