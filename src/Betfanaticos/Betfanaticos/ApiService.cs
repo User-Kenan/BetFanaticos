4326c9608d3030e974698ba2bd89ebe514f7189e
Dell <Dell@DESKTOP-4MOTUUD>
2026-05-28 15:09:33 +0200

    Bet Klasse fast Fertig und API Service angefangen

---
+++ src/Betfanaticos/Betfanaticos/ApiService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Betfanaticos
+{
+    public class ApiService
+    {
+        private readonly HttpClient client = new HttpClient();
+
+        public GetMatches()
+        {
+
+        }
+
+
+    }
+}
